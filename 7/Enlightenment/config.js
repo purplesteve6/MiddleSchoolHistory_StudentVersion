0331912d14c8729a03fbbd6f2b74badf6f7d52bd
purplesteve6 <purplesteve6@gmail.com>
2026-01-28 09:55:20 -0500

    updates

--- 7/Enlightenment/config.js
+++ 7/Enlightenment/config.js
@@ -3,7 +3,7 @@ window.TOPIC_CONFIG = {
   title: "The Enlightenment",
   subtitle: "Middle School History",
   homeLabel: "The Enlightenment Home",
-  homeHref: "/7/Enlightenment/",
-  badgeIcon: "/assets/images/icons/7_iconbk.png",
-  mainLogo: "/assets/images/logo/MSHistory_Logo_Basic_Web.png"
+  homeHref: "7/Enlightenment/",
+  badgeIcon: "assets/images/icons/7_iconbk.png",
+  mainLogo: "assets/images/logo/MSHistory_Logo_Basic_Web.png"
 };
