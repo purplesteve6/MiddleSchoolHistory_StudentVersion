3317579d07c23ad8ecedbc1d85bff2ce8ada5076
purplesteve6 <purplesteve6@gmail.com>
2026-01-28 12:36:26 -0500

    removed slashes

--- 6/GreekMythology/config.js
+++ 6/GreekMythology/config.js
@@ -3,7 +3,7 @@ window.TOPIC_CONFIG = {
   title: "Greek Mythology",
   subtitle: "Middle School History",
   homeLabel: "Greek Mythology Home",
-  homeHref: "/6/GreekMythology/",
-  badgeIcon: "/assets/images/icons/6_icon.png",
-  mainLogo: "/assets/images/logo/MSHistory_Logo_Basic_Web.png"
+  homeHref: "6/GreekMythology/",
+  badgeIcon: "assets/images/icons/6_icon.png",
+  mainLogo: "assets/images/logo/MSHistory_Logo_Basic_Web.png"
 };
